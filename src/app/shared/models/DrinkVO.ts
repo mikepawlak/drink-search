78483522caa6de68040908d8ebda7abe3e71974e
mikepawlak <mpawlak719@gmail.com>
2019-11-05 19:52:22 -0600

    add typings

--- src/app/shared/models/DrinkVO.ts
+++ src/app/shared/models/DrinkVO.ts
@@ -1,3 +1,13 @@
-export interface DrinkVO {
+import { Ingredient } from './Ingredient';
 
+export interface DrinkVO {
+    id: number,
+    name: string,
+    tags: string,
+    category: string,
+    alcoholic: string,
+    glass: string,
+    instructions: string,
+    drinkThumb: string,
+    ingredients: Ingredient[],
 }
