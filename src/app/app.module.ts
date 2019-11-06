7de8da1ea7f58277861ff9e7e8fe3800f6032217
mikepawlak <mpawlak719@gmail.com>
2019-11-05 18:51:19 -0600

    app stub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/home/search/search.component';
+import { ListComponent } from './components/home/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
